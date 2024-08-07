89d70806ff19ee8d100688084d6e21ea6197eecb
Narendrakumar432 <kumarenarendra97322@gmail.com>
2024-08-07 01:51:40 -0400

    scope

---
+++ scope_lab.js
@@ -0,0 +1,10 @@
+
+{
+var globalvar = "I am a global variable";
+let globlet = "I am also a global , but scoped with let";
+const globalConst = "I am global constant";
+console.log(globalvar); 
+
+console.log(globalConst);
+
+}
